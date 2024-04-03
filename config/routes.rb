a6ef93200e88821ba606b8eac9edb7332918682e
sslevel5 <sslevel5@gmail.com>
2024-04-03 15:30:34 +0000

    routing追加_20240404 #6

--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,33 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # 顧客側
+  post '/customers/sign_in', to: 'public/sessions#create'
+  get 'customers/my_page', to: 'public/customers#show', as: 'public_customers'
+  get 'customers/information/edit', to: 'public/customers#edit', as: 'public_customers_information_edit'
+  # 退会処理
+  get '/customers/unsubscribe', to: 'public/customers#unsubscribe', as: 'public_customers_unsubscribe'
+  patch '/customers/withdraw', to: 'public/customers#withdraw'
 
+  # 管理者側
+  get '/admin', to: 'admin/homes#top', as: 'admin_root'
 
-  namespace :admin do
-    resources :cards
+  namespace :public, path: '' do
+    resources :cards, except: [:destroy]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :rooms
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :registrations, only: [:new, :create]
   end
 
-  namespace :public do
-    resources :cards
+  namespace :admin do
+    resources :cards, only: [:index, :show, :edit, :update]
+    resources :raritys, only: [:index, :create, :edit, :update, :show]
+    resources :stores, only: [:index, :create, :edit, :update, :show]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :rooms
+    resources :homes, only: [:top]
+    resources :sessions, only: [:new, :create]
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_02_085219) do
+ActiveRecord::Schema.define(version: 2024_04_02_223407) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,35 @@ ActiveRecord::Schema.define(version: 2024_04_02_085219) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "card_comments", force: :cascade do |t|
+    t.text "comment", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contact_messages", force: :cascade do |t|
+    t.text "message", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,6 +94,36 @@ ActiveRecord::Schema.define(version: 2024_04_02_085219) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "letter_room_messages", force: :cascade do |t|
+    t.text "message", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "letter_rooms", force: :cascade do |t|
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "nices", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rarities", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
