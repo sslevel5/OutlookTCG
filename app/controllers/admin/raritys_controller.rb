79cff9f1dcb1c327bd46d7d0259be59547546a2e
sslevel5 <sslevel5@gmail.com>
2024-04-04 18:20:51 +0000

    レアリティ、店舗追加_20240405 #7

--- app/controllers/admin/raritys_controller.rb
+++ app/controllers/admin/raritys_controller.rb
@@ -1,2 +1,47 @@
 class Admin::RaritysController < ApplicationController
+before_action :authenticate_admin!
+
+  def index
+    @raritys = Rarity.all
+    @rarity = Rarity.new
+  end
+
+  def show
+    redirect_to edit_admin_rarity_path(params[:id])
+  end
+
+  def create
+    @rarity = Rarity.new(rarity_params)
+    @raritys = Rarity.all
+    if @rarity.save
+      flash[:notice] = "ジャンルを追加しました。"
+      redirect_to admin_raritys_path
+    else
+      flash.now[:alert] = "ジャンルの追加に失敗しました。"
+      render :index
+    end
+  end
+
+  def edit
+    @rarity = Rarity.find(params[:id])
+  end
+
+
+  def update
+    @rarity = Rarity.find(params[:id])
+    if @rarity.update(rarity_params)
+      flash[:notice] = "ジャンルを変更しました。"
+      redirect_to admin_raritys_path
+    else
+      flash.now[:alert] = "ジャンルの変更に失敗しました。"
+      render :edit
+    end
+  end
+
+
+private
+
+  def rarity_params
+    params.require(:rarity).permit(:name)
+  end
 end
--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,2 +1,47 @@
 class Admin::StoresController < ApplicationController
+before_action :authenticate_admin!
+
+  def index
+    @stores = Store.all
+    @store = Store.new
+  end
+
+  def show
+    redirect_to edit_admin_store_path(params[:id])
+  end
+
+  def create
+    @store = Store.new(store_params)
+    @stores = Store.all
+    if @store.save
+      flash[:notice] = "ジャンルを追加しました。"
+      redirect_to admin_stores_path
+    else
+      flash.now[:alert] = "ジャンルの追加に失敗しました。"
+      render :index
+    end
+  end
+
+  def edit
+    @store = Store.find(params[:id])
+  end
+
+
+  def update
+    @store = Store.find(params[:id])
+    if @store.update(store_params)
+      flash[:notice] = "ジャンルを変更しました。"
+      redirect_to admin_stores_path
+    else
+      flash.now[:alert] = "ジャンルの変更に失敗しました。"
+      render :edit
+    end
+  end
+
+
+private
+
+  def store_params
+    params.require(:store).permit(:name)
+  end
 end
