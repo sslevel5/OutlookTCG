24e4609a0657bacc280d8641b18b234976c1413c
sslevel5 <sslevel5@gmail.com>
2024-04-06 14:47:44 +0000

    コメント機能_20240406 #9

--- app/controllers/admin/card_comments_controller.rb
+++ app/controllers/admin/card_comments_controller.rb
@@ -1,2 +1,16 @@
 class Admin::CardCommentsController < ApplicationController
+
+  def create
+    card = card.find(params[:card_id])
+    comment = current_user.card_comments.new(card_comment_params)
+    comment.card_id = card.id
+    comment.save
+    redirect_to public_card_path(card)
+  end
+
+  private
+
+  def card_comment_params
+    params.require(:card).permit(:comment, :is_active)
+  end
 end
--- app/controllers/public/cards_controller.rb
+++ app/controllers/public/cards_controller.rb
@@ -27,6 +27,7 @@ class Public::CardsController < ApplicationController
 
   def show
    @card = Card.find(params[:id])
+   @card_comment = CardComment.new
   end
 
   def edit
