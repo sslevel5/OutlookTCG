427145d92e794278295019c53437c7cf595da604
sslevel5 <sslevel5@gmail.com>
2024-04-06 07:25:37 +0000

    routing修正_20240406 #7

--- config/routes.rb
+++ config/routes.rb
@@ -2,29 +2,21 @@ Rails.application.routes.draw do
   root to: 'public/homes#top'
   get '/about', to: 'public/homes#about', as: 'about'
   get '/home', to: 'public/homes#home', as: 'home'
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 
-  # 顧客側
   post '/customers/sign_in', to: 'public/sessions#create'
-  get 'customers/information/edit', to: 'public/customers#edit', as: 'public_customers_information_edit'
-  # 退会処理
+  get 'customers/information/:id/edit', to: 'public/customers#edit', as: 'edit_public_customer_information'
   get '/customers/unsubscribe', to: 'public/customers#unsubscribe', as: 'public_customers_unsubscribe'
   patch '/customers/withdraw', to: 'public/customers#withdraw'
 
-  # 管理者側
   get '/admin', to: 'admin/homes#top', as: 'admin_root'
 
   namespace :public, path: '' do
@@ -43,8 +35,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     resources :stores, only: [:index, :create, :edit, :update, :show]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :rooms
-    resources :homes, only: [:top]
+    resources :homes, only: [:top, :home]
     resources :sessions, only: [:new, :create]
   end
-
 end
