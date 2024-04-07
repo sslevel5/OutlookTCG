8293586df44200c752f5c570aa9d41aceb3a967c
sslevel5 <sslevel5@gmail.com>
2024-04-07 11:44:13 +0000

    微修正_20240407 #7

---
+++ app/controllers/public/favorites_controller.rb
@@ -0,0 +1,16 @@
+class Public::FavoritesController < ApplicationController
+
+  def create
+    card = Card.find(params[:card_id])
+    favorite = current_customer.favorites.new(card_id: card.id)
+    favorite.save
+    redirect_to public_card_path(card)
+  end
+
+  def destroy
+    card = Card.find(params[:card_id])
+    favorite = current_customer.favorites.find_by(card_id: card.id)
+    favorite.destroy
+    redirect_to public_card_path(card)
+  end
+end
