138002904daa60f5f5632d4e7a199f4d7bf79c4a
sslevel5 <sslevel5@gmail.com>
2024-04-07 14:32:37 +0000

    ゲストログイン機能_20240407 #11

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Customers::SessionsController < Devise::SessionsController
   def guest_sign_in
     customer = Customer.guest
-    sign_in Customer
+    sign_in customer
     redirect_to public_customer_path(customer), notice: "guestuserでログインしました。"
   end
 end
