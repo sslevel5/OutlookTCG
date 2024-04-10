6de6775402998ce41f25dd881fce594fb0d8bc66
sslevel5 <sslevel5@gmail.com>
2024-04-10 09:20:20 +0000

    マイグレーション修正_20240410

--- db/migrate/20240402223351_create_letter_rooms.rb
+++ db/migrate/20240402223351_create_letter_rooms.rb
@@ -3,7 +3,7 @@ class CreateLetterRooms < ActiveRecord::Migration[6.1]
     create_table :letter_rooms do |t|
 
       t.boolean :is_active, null: false, default: true
-
+      t.integer :customer_id, null: false
       t.timestamps
     end
   end
