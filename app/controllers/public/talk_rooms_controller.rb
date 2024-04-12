4f937941f33e86802cf60ebbc4e5d0bde5d5d73a
sslevel5 <sslevel5@gmail.com>
2024-04-12 03:38:23 +0000

    DM機能試行錯誤中_20240412 #15

--- app/controllers/public/talk_rooms_controller.rb
+++ app/controllers/public/talk_rooms_controller.rb
@@ -2,16 +2,22 @@ class Public::TalkRoomsController < ApplicationController
   before_action :authenticate_customer!
   
   def create
+    @raritys = Rarity.all
+    @stores = Store.all
     recipient = Customer.find(params[:customer_id])
     @talk_room = TalkRoom.find_or_create_by(sender_id: current_customer.id, recipient_id: recipient.id)
     redirect_to @talk_room
   end
 
   def index
+    @raritys = Rarity.all
+    @stores = Store.all
     @talk_rooms = TalkRoom.where("sender_id = ? OR recipient_id = ?", current_customer.id, current_customer.id)
   end
 
   def senders
+    @raritys = Rarity.all
+    @stores = Store.all
     customer = Customer.find(params[:id])
     @customers = customer.sending_customers
   end
