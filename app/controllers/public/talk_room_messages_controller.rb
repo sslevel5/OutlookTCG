798aa320ebacf7e4b2452aba2772ce8172d2ee1f
sslevel5 <sslevel5@gmail.com>
2024-04-14 06:28:32 +0000

    DM機能試行錯誤中_20240413 #15

--- app/controllers/public/talk_room_messages_controller.rb
+++ app/controllers/public/talk_room_messages_controller.rb
@@ -8,18 +8,18 @@ class Public::TalkRoomMessagesController < ApplicationController
     @message = @talk_room.talk_room_messages.build(message_params)
     @message.customer = current_customer
     against_customer_id = @talk_room.sending?(current_customer) ? @talk_room.recipient_id : @talk_room.sender_id
-    if @message.save
-      redirect_to talk_rooms_path(@talk_room.id, against_customer_id: against_customer_id)
-    else
-      @against_customer = @talk_room.opposite_customer(current_customer)
-      render :show
-    end
+    @message.save
+    redirect_to talk_rooms_path(@talk_room.id, against_customer_id: against_customer_id)
   end
 
   def update
     @message = TalkRoomMessage.find(params[:id])
+    @talk_room = @message.talk_room # メッセージに関連するトークルームを取得
+    # メッセージが属するトークルームから受信者のIDを取得
+    against_customer_id = @talk_room.recipient_id
+    @against_customer = Customer.find(against_customer_id)
     @message.update(message_params)
-    redirect_to talk_rooms_path(@message.talk_room_id, against_customer_id: @message.talk_room.opposite_customer(current_customer).id)
+    redirect_to talk_rooms_path(id: @talk_room.id, against_customer_id: against_customer_id)
   end
 
   private
