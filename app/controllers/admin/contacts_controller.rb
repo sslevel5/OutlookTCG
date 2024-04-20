02fd3135953e422cb7a4b869a2681d5e1d5b67e9
sslevel5 <sslevel5@gmail.com>
2024-04-20 09:10:40 +0000

    管理者側_20240420 #26

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -16,17 +16,25 @@ class Admin::ContactsController < ApplicationController
   def edit
     @raritys = Rarity.all
     @stores = Store.all
+    @contact = Contact.find(params[:id])
   end
 
   def update
-
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+     flash[:notice] = "状態を変更しました。"
+     redirect_to admin_contact_path(@contact)
+    else
+     flash.now[:alert] = "状態の変更に失敗しました。"
+     render :edit
+    end
   end
 
 
   private
 
   def contact_params
-    params.require(:contact).permit(:title, :message)
+    params.require(:contact).permit(:title, :message, :is_active)
   end
 
 end
