a8552ac059a8362c3a83737cfe3dddea2a2d4112
sslevel5 <sslevel5@gmail.com>
2024-04-25 13:56:27 +0000

    微修正_20240425

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :set_common_variables
 
   # 404エラーのハンドリング
-  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_not_found
+
+
+  def render_not_found
+    render file: "#{Rails.root}/portfolio/app/views/layouts/notfind.html.erb", layout: false, status: :not_found
+  end
 
   private
 
   def set_common_variables
     @raritys = Rarity.all
     @stores = Store.all
-  end
-
-  def render_404
-    render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
   end
 end
