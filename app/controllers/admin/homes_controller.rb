a364edd237da73f84c407fd329e72a8973a3dacb
sslevel5 <sslevel5@gmail.com>
2024-04-25 13:36:25 +0000

    微修正_20240425

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,7 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-
+    @cards = Card.order(created_at: :desc).search(params[:q]).by_rarity(params[:rarity_id]).by_store(params[:store_id])
   end
 
   def home
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :set_common_variables
+  before_action :set_common_variables
+
+  # 404エラーのハンドリング
+  rescue_from ActionController::RoutingError, with: :render_404
 
   private
 
   def set_common_variables
     @raritys = Rarity.all
     @stores = Store.all
+  end
+
+  def render_404
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
   end
 end
