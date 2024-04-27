8f215ec324224ac83af7409e9be75e3f87f2b2d8
sslevel5 <sslevel5@gmail.com>
2024-04-27 05:19:59 +0000

    微修正_20240427

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :set_common_variables
 
+  def notfind
+    render 'layouts/notfind'
+  end
+
   private
 
   def set_common_variables
