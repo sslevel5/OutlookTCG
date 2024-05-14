7cc20386e3630802d5a15c708532697a4c7e9f2b
sslevel5 <sslevel5@gmail.com>
2024-05-14 02:24:05 +0000

    退会処理修正_20240513

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -35,7 +35,8 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
     @customer = current_customer
-    @customer.update(is_deleted: true, is_active: false) # 会員ステータスを退会に変更
+    withdrew_email = "withdrew_" + Time.now.to_i.to_s + @customer.email
+    @customer.update(email: withdrew_email, is_active: false) # 会員ステータスを退会に変更
     reset_session
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
